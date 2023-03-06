b6511c65dda478eadc0a3bb35b9b1983ab2f3d4a
joseph akah <josephakah@macbook-pro-2.lan>
2023-03-06 13:19:19 +0000

    updated with index template json css

--- starter/index.js
+++ starter/index.js
@@ -1,15 +1,75 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+// import npm packages
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// Import classes
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
-const render = require("./src/page-template.js");
+// Output Directory
+const DIST_DIR = path.resolve(__dirname, 'output');
+// Output file path and name
+const outputPath = path.join(DIST_DIR, 'team.html');
+
+// Import HTML template
+const templateHTML = require('./src/templateHTML');
+
+// Create an empty array of team members
+const teamMembers = [];
 
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+function addTeamMember() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'what_team_member',
+                message: 'Add an engineer, Add an intern or finish assembling your team?',
+                choices: ['Engineer', 'Intern', 'Finish building Team!'],
+            },
+        ])
+        .then((val) => {
+            if (val.what_team_member === 'Engineer') {
+                addEngineer();
+            } else if (val.what_team_member === 'Intern') {
+                addIntern();
+            } else {
+                createTeamFile();
+            }
+        });
+}
+
+function addManager() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of the team manager?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Employee ID of the team manager?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Email address of the team manager?',
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'What is the office number of the team manager?',
+            },
+        ])
+        .then((val) => {
+            const manager = new Manager(val.name, val.id, val.email,val.officeNumber);
+            console.table(manager);
+            teamMembers.push(manager);
+            addTeamMember();
+        });
+}
 
